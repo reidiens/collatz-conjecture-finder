727095e3055864d4131111711d10b1ae256ba37d
ruby :3 <124638816+reidiens@users.noreply.github.com>
2023-03-02 10:30:47 -0500

    Update main.c
    Deglobalized the "count" variable

--- main.c
+++ main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+
 #define EVENOP a/2              /* what to do if even/odd */ 
 #define ODDOP a*3+1             /*                        */ 
 
 
-int count;                      // iteration counter
-
-void collatz(long int a) {      // function that does all the math
-    printf("\n# %6lu", a);
+// function that does all the math
+int collatz(long int a) {      
+    int count = 0;             // iteration counter 
+    printf("\n# %7lu", a);
     while (a > 1) {             
         if (a % 2 == 0) {       // check if a is even
             count++;    
@@ -16,8 +17,8 @@ void collatz(long int a) {      // function that does all the math
             count++;
             printf("\n# %7lu", a = ODDOP);
         }
-        
     }
+    return count;
 }
 
 int main() {
@@ -33,6 +34,6 @@ int main() {
         goto inp;                   
     }
     org = a;                        // store original value
-    collatz(a);
-    printf("\nThe number %lu takes %d iterations to reach 1.", org, count);
+    int iter = collatz(a);          // store the number of iterations
+    printf("\nThe number %lu takes %d iterations to reach 1.", org, iter);
 }
