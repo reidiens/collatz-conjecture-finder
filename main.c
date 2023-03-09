e68b5ae8ad492c165f0533fd86ad70ee918f9a68
ruby :3 <124638816+reidiens@users.noreply.github.com>
2023-03-09 18:53:29 -0500

    Restart, QOL fixes
    Added a way to restart the program, reorganized some lines, and added more descriptive variable names

--- main.c
+++ main.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 #define EVENOP a/2              /* what to do if even/odd */ 
 #define ODDOP a*3+1             /*                        */ 
 
+#define SIGCHARS 1              // num. of significant chars for strncmp
+
 
 // function that does all the math
 int collatz(long int a) {      
-    int count = 0;             // iteration counter 
-    printf("\n# %7lu", a);
+    int count = 0;              // iteration counter 
+    printf("\n# %7lu", a);      // format to 7 spaces to account for large nums
     while (a > 1) {             
-        if (a % 2 == 0) {       // check if a is even
-            count++;    
-            printf("\n# %7lu", a = EVENOP);
+        if (a % 2 == 0) {       // check if a (start_num) is even
+            count++;            
+            printf("\n# %7lu", a = EVENOP);     // print & store new value at the same time
         }     
         else {
             count++;
@@ -22,18 +26,32 @@ int collatz(long int a) {
 }
 
 int main() {
-    inp:                            // restart point if error is encountered
-    long int a, org;
+    system("clear");                 // clear the terminal on startup
+
+    init:                            // restart point if error is encountered
+    long int start_num, org_num;     // starting number; original number   (long int to account for large values)     
+
     printf("Enter a value:\n> ");
-    scanf("%lu", &a);
+    scanf("%lu", &start_num);
     getchar();
-    long int* b = &a;               // pointer for error checking
-    if (a <= 0) {                   // checks for input error
+    org_num = start_num;                        // store original value
+
+    if (start_num <= 0) {                   // checks for input error
         printf("\n** Please enter a positive, non-zero integer.\n\n");
-        b = NULL;                   // deinitialize a (so that error check works multiple times)
-        goto inp;                   
+        goto init;                   
     }
-    org = a;                        // store original value
-    int iter = collatz(a);          // store the number of iterations
-    printf("\nThe number %lu takes %d iterations to reach 1.", org, iter);
+
+    int iter = collatz(start_num);          // store the number of iterations (iter = iterations)
+    printf("\nThe number %lu takes %d iterations to reach 1.", org_num, iter);
+
+    printf("\n\nGo again? ");
+    char res[10];                     // 10 is an arbitrary value (res = restart)
+    scanf("%s", res);
+    printf("\n");                   // give space b/w "Go again?" text and "Enter value" text if "y" is input
+    getchar();
+    
+    if (strncmp(res, "yes\n", SIGCHARS) == 0 || strncmp(res, "Yes\n", 1) == 0) {
+        org_num = 0;
+        goto init;
+   }
 }
